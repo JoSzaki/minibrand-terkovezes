38e45aaf1d1f4104ab1e6a0ad407641ee7cf6d99
biogyerek <5813489+biogyerek@users.noreply.github.com>
2025-09-04 22:42:25 +0200

    Fix navigation links by adding trailing slash to base path

--- live/astro.config.mjs
+++ live/astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://joszaki.github.io',
-  base: '/minibrand-terkovezes',
+  base: '/minibrand-terkovezes/',
   output: 'static',
   integrations: [
     tailwind()
